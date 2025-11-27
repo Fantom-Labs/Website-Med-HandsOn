0fd1358f1b6477e99323f83762da75c2af790f37
Fantom-Labs <fantom.weblabs@gmail.com>
2025-11-27 14:14:49 -0300

    fix: Instala dependÃªncia faltante e corrige carregamento do Studio (SSR false)

--- src/app/studio/[[...index]]/page.tsx
+++ src/app/studio/[[...index]]/page.tsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity/sanity.config'
+import dynamic from 'next/dynamic'
+
+// Carrega o Studio apenas no cliente (SSR: false) para evitar erros de build
+// como "Module not found: Can't resolve 'sanity/router'"
+const NextStudio = dynamic(
+  () => import('next-sanity/studio').then((mod) => mod.NextStudio),
+  { ssr: false }
+)
 
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
-
